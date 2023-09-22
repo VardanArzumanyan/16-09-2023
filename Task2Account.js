5ea36b1897f58bd3c45c40caaefc07fd3920b260
VardanArzumanyan <vardan.arzumanyan.123@gmail.com>
2023-09-23 02:51:13 +0400

    Account

---
+++ Task2Account.js
@@ -0,0 +1,65 @@
+class Account {
+  static accounts = [];
+  constructor(name, balance) {
+    let id = 0;
+    this.id = id++;
+    this.name = name;
+    this._balance = balance;
+    Account.accounts.push(this);
+  }
+
+  get balance() {
+    return this._balance;
+  }
+
+  set balance(num) {
+    if (typeof num === "number" && num >= 0) {
+      this._balance = num;
+    } else {
+      console.log("Invalid balance value");
+    }
+  }
+
+  credit(num) {
+    if (typeof num === "number" && num > 0) {
+      this.balance += num;
+      console.log(`Credit ${num} to ${this.name}`);
+    } else {
+      console.log("Invalid money");
+    }
+  }
+
+  debit(num) {
+    if (typeof num === "number" && num > 0) {
+      this.balance -= num;
+      console.log(`Debit ${num} from ${this.name}`);
+    } else {
+      console.log("Invalid money");
+    }
+  }
+
+  transferTo(pers, num) {
+    if (typeof num === "number" && num > 0 && num <= this.balance) {
+      this.balance -= num;
+      pers.credit(num);
+      console.log(`Transferred ${num} from ${this.name} to ${pers.name} `);
+    } else {
+      console.log("Inavlid money for transfer");
+    }
+  }
+
+  static identifyAccounts(...accounts) {
+    return accounts.map((account) => account.id);
+  }
+}
+
+const saving = new Account("saving", 1000);
+const current = new Account("current", 8000);
+saving.credit(5000);
+saving.debit(1000);
+saving.debit(2000);
+saving.transferTo(current, 1000);
+console.log(saving.balance);
+console.log(current.balance);
+const res = Account.identifyAccounts(current, saving);
+console.log(saving.balance);
